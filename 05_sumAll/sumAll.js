c47d9152337ec46ffe4eb5100758e88e0f49c233
lewisgunson <22944704+lewisgunson@users.noreply.github.com>
2024-02-10 15:14:24 +0000

    Added completed exercises for 5 and 6

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,44 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let sum = 0;
+    // const min = Math.min(start, end);
+    // const max = Math.max(start, end);
+    
+    if (start < end) {
+    //     start = Math.min(start, end);
+    //     end = Math.max(start, end);
+    // } else {
+    //     start = Math.max(start, end);
+    //     end = Math.min(start, end);
+    // }
 
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+} else {
+    for (let i = end; i <= start; i++) {
+        sum += i;
+}
+}
+    // for (let i = min; i <= max; i++) {
+    //     sum += 1;
+    // }
+
+    if (typeof start === 'number' && typeof end === 'number' && sum > 0) {
+        return sum;
+    } else {
+        return 'ERROR';
+    }
 };
+
+let defaultResults = sumAll();
+let numWithinRange = sumAll(1,4);
+let largeNum = sumAll(1,4000);
+let largeNumFirst = sumAll(123, 1);
+let negativeNumers = sumAll(-10, 4);
+let nonNum = sumAll(10, "90");
+let moreNonNums = sumAll(sumAll(10, [90, 1]));
+
+console.log(moreNonNums);
 
 // Do not edit below this line
 module.exports = sumAll;
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,47 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
-};
+    if (year % 4 === 0 && year % 100 !== 0) {
+        return true;
+        } else if (year % 4 === 0 && year % 100 === 0 && year % 400 === 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+const nonCent = 1996;
+const nonCentFalse = 1997;
+const nonCentFuture = 34992;
+const centYears = 1900;
+const centYears1600 = 1600;
+const centYears700 = 700;
+
+console.log(leapYears(centYears700));
+
+    // if (year < 400) {
+    // } else {
+    //     return false;
+    //     }
+    // } else if (year > 400) {
+        // if (Number.isInteger(year / 4 && year % 100 !== 0 && year / 400)) {
+        //     return true;
+
+/*
+divides by 4
+and is a whole number (not decimal)
+
+& does not divide by 100
+and is a whole number (not decimal)
+unless
+they are also divisible by 400
+---
+if divible by 4 and not divisble by 400
+else
+if divisble 400 and disible by 100
+else
+fail
+*/
+
 
 // Do not edit below this line
 module.exports = leapYears;
